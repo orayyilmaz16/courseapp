71374113f0e84915df9b405ccb710ff3bcdb2127
Oray Yılmaz <orayyilmaz16@gmail.com>
2024-11-20 23:29:20 +0300

    bir değişiklik daha yapıldı

--- courses/urls.py
+++ courses/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 from . import views
 
 
-
-
 urlpatterns = [
     path('',views.kurslar),
     path('list',views.kurslar),
     path('<kurs_adi>',views.details),
     path('<int:category_id>', views.getCoursesByCategoryId),  
     path('<str:category_name>',views.getCoursesByCategory),
-   
+
 ]
